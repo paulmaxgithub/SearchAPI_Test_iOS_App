377579c1a9a86b0da688887b38b914832ed19b3a
PAULMAX <ipaulmax@icloud.com>
2020-08-05 23:31:46 +0300

    Initial Commit

---
+++ SearchTestProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SearchTestProject
+//
+//  Created by Paul Max on 8/5/20.
+//  Copyright © 2020 PAULMAX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
